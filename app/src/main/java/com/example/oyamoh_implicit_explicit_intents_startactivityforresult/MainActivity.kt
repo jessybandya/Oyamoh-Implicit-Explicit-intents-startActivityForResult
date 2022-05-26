37bb9dee3d8a6efe336b4daae213e8325383b018
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-05-26 22:47:50 +0300

    done

--- app/src/main/java/com/example/oyamoh_implicit_explicit_intents_startactivityforresult/MainActivity.kt
+++ app/src/main/java/com/example/oyamoh_implicit_explicit_intents_startactivityforresult/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.oyamoh_implicit_explicit_intents_startactivityforresult
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.oyamoh_implicit_explicit_intents_startactivityforresult.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+
+class MainActivity : AppCompatActivity()  {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,7 +21,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.apply {
-
+            button1.setOnClickListener{
+                val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                startActivityForResult(intent, 2)
+            }
         }
     }
+
+    // Call Back method  to get the Message form other Activity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        binding.apply {
+            // check if the request code is same as what is passed  here it is 2
+            if (requestCode == 2) {
+                val message = data?.getStringExtra("MESSAGE")
+                textView1.text = message
+            }
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/oyamoh_implicit_explicit_intents_startactivityforresult/SecondActivity.kt
+++ app/src/main/java/com/example/oyamoh_implicit_explicit_intents_startactivityforresult/SecondActivity.kt
@@ -1,11 +1,32 @@
 package com.example.oyamoh_implicit_explicit_intents_startactivityforresult
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.example.oyamoh_implicit_explicit_intents_startactivityforresult.databinding.ActivityMainBinding
 
-class SecondActivity : AppCompatActivity() {
+
+class SecondActivity : Activity() {
+    var editText1: EditText? = null
+    var button1: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        editText1 = findViewById<View>(R.id.editText1) as EditText
+        button1 = findViewById<View>(R.id.button1) as Button
+
+        button1!!.setOnClickListener {
+            val message = editText1!!.text.toString()
+            val intent = Intent()
+            intent.putExtra("MESSAGE", message)
+            setResult(2, intent)
+            finish() //finishing activity
+        }
     }
+
+
 }
